bcb2e5efdcc1aee625aa594f8cdf67d6caa3f988
jbrasko <jbrasko0508@gmail.com>
2021-10-25 14:48:11 -0500

    update config for ts

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'index_bundle.js',
   },
+  devtool: 'sourcemap',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(ts|js)x?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
